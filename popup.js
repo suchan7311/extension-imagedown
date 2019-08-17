cec79aa4b82e5b0ab4c340e9a4d7cc59b2aeb601
suchan7311 <suchan7311@naver.com>
2019-08-17 18:51:42 +0900

    팝업창 버튼 생성

---
+++ popup.js
@@ -0,0 +1,4 @@
+document.write("<button id='executebutton' type='button'  style='width:100px;height:50; color: blue; background-color: white'>이미지다운</button>");
+var button = document.getElementById('executebutton');
+button.onclick = function() {
+};
